4789e1a635bccc25ad6a0535aa556b36b3e5645f
lucas <lucas.salinas119@gmail.com>
2020-10-05 11:39:43 +0200

    modifs

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-document.addEventListener('DOMContentLoaded', function(){
-    
-    let addButton = document.querySelector("#plus");
+var laListe = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < laListe.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("fermer");
+  span.className = "fermer";
+  span.appendChild(txt);s
+  laListe[i].appendChild(span);
+  localStorage.setItem(laListe[i]);
+}
+
+var close = document.getElementsByClassName("fermer");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
 
-    addButton.addEventListener("click", function() {
+function nouvelleAct() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("activ").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Aucune activité !");
+  } else {
+    document.getElementById("UL").appendChild(li);
+  }
+  document.getElementById("activ").value = "";
 
-        let newItem = document.getElementById("liste").value;
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("fermer");
+  span.className = "fermer";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-        if (newItem) {
-
-            addItemTodo(newItem);
-  
-            document.getElementById("liste").value = "";
-        }
-    });
- 
-
-    function addItemTodo(text) {
-
-        let list = document.getElementById("todo");
- 
-        let item = document.createElement('li');
-      
-        item.innerText = text;
-
-        let buttons = document.createElement('div');
-        buttons.classList.add("buttons");
-
-
-        let remove = document.createElement('button');
-        remove.classList.add('remove');
-
-        remove.addEventListener("click", removeItem);
-
-   
-    
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-
-
-    function removeItem() {
-        
-        let item = this.parentNode.parentNode;
-
-        let parent = item.parentNode;
-
-        parent.removeChild(item);
-    }
-})
+  }
+} 
